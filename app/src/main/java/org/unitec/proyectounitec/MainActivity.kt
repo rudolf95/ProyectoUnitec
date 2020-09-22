bf3d6bf2b33cc532fb379bbb574b07efaa4ee500
rudolf95 <55564364+rudolf95@users.noreply.github.com>
2020-09-22 18:42:53 -0500

    Update MainActivity.kt

--- app/src/main/java/org/unitec/proyectounitec/MainActivity.kt
+++ app/src/main/java/org/unitec/proyectounitec/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 //el punto de partida de toda app es la MainActivity
 class MainActivity : AppCompatActivity() {
     //fun en kotlin es una palabra reservada para implementar o crear una funcion o metodo
-    //vamos a probar que funciona github
+    //vamos a probar que funciona github esto lo estoy edi8tando desde el repositorio
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
