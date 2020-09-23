5b603d90388bc3ec5412156e32385dede5b2ff04
rudolf95 <hinojosarodolfo@hotmail.com>
2020-09-22 19:52:38 -0500

    fin de la clase

--- app/src/main/java/org/unitec/proyectounitec/MainActivity.kt
+++ app/src/main/java/org/unitec/proyectounitec/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     //vamos a probar que funciona github esto lo estoy edi8tando desde el repositorio
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.ejemplito)
     }
 }
